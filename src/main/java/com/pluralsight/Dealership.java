2456c04a853e40cd5581d6a638f9f02051181050
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-17 16:32:16 -0500

    add getters and setters and constructors added new arraylist for inventory

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -11,4 +11,50 @@ public class Dealership{
 
     //add inventory extended by vehicle
     private ArrayList<Vehicle> inventory;
+
+    //add constructors
+
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+
+        //add array list to inventory
+        this.inventory = new ArrayList<>();
+    }
+
+    //add getters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
 }
