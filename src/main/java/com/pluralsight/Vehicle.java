882de5223b03bd38f4313c58379e02f056989c78
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-17 16:14:24 -0500

    added variables for dealership and vehicle

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Vehicle {
+
+    //add arguments to hold info about a specific website
+    private Integer vin;
+    private Integer year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private Integer odometer;
+    private Double price;
 }
