5ca7e7f70021006b5f1dbe256c67d6199ca392ed
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-17 18:00:17 -0500

    set dealership to empty and add pla

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,11 +1,27 @@
 package com.pluralsight;
 
+import java.io.File;
+
 public class DealershipFileManager {
 
 //get dealership(): Dealership
 
+    //add file name
+    private static String File_Name = "inventory.csv";
+
 
     //save dealership(dealership)
+    public Dealership getDealership() {
+        Dealership dealership = null;
+
+        //add try to add reader
+
+        //parse data by pipes using indexes
+
+        //add the formatting for new vehicles
+
+        //add catch exceptions (exception e)
+    }
 
 
     //to string for reference while constructing dealership formatting
